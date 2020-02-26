cccb348c9d425c8e3a141be855455f40987e6050
David Nguyen <hpcsc@users.noreply.github.com>
2020-02-26 22:54:05 +1100

    add initial new-matching route

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,32 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket::request::LenientForm;
+
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "Pair Matching Bot"
+}
+
+#[derive(FromForm,Debug)]
+struct NewMatchingRequest {
+    channel_id: String,
+    channel_name: String,
+    user_id: String,
+    user_name: String,
+    command: String,
+    text: String,
+    response_url: String,
+}
+
+#[post("/new-matching", data = "<new_matching_request>")]
+fn new_matching(new_matching_request: LenientForm<NewMatchingRequest>) -> String {
+    format!("Received: {:?}", new_matching_request)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![
+        index,
+        new_matching
+    ]).launch();
 }
